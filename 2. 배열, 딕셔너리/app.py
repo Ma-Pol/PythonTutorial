6e0757d03a48b61585d9f3a4a20a1cdc929720f8
Ma-Pol <ysh.skyblue6485@gmail.com>
2023-04-16 20:09:04 +0900

    #2 배열, 딕셔너리

---
+++ 2. 배열, 딕셔너리/app.py
@@ -0,0 +1,28 @@
+# 배열
+oldCar = ["K5", "white", [5000, 6000, 10000]]
+
+print(oldCar)
+print(oldCar[0])
+print(oldCar[1])
+print(oldCar[2])
+print(oldCar[2][0])
+print()
+
+oldCar[1] = "black"
+print(oldCar[1])
+print()
+
+# oldCar.sort()
+# oldCar.reverse()
+# oldCar.append('red')
+# oldCar.pop()
+# oldCar.insert(1, 'blue')
+
+# 딕셔너리
+oldCar2 = {"brand": "BMW", "model": "520d", "price": 5000}
+
+print(oldCar2)
+print(oldCar2["brand"])
+print(oldCar2["model"])
+print(oldCar2["price"])
+print()

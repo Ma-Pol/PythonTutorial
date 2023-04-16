67179468ff96bbb9d2f942b9f66836cc4dbad519
Ma-Pol <ysh.skyblue6485@gmail.com>
2023-04-16 22:01:40 +0900

    #5 함수

---
+++ 5. 함수/app.py
@@ -0,0 +1,28 @@
+def 인사말(name):
+    print(name + "님, 안녕하세요")
+
+
+인사말("김철수")
+인사말("이승현")
+
+
+def 증가(num):
+    print(num + 1)
+
+
+증가(1)
+증가(2)
+
+
+def 함수():
+    return 10
+
+
+print(함수())
+
+
+def 덧셈(num1, num2):
+    print(num1 + num2)
+
+
+덧셈(100, 2000)
